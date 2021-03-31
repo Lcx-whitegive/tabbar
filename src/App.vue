adb8e4c974ebc965c7ffa0f69862301930d3e56e
Lcx-whitegive <1758811566@qq.com>
2021-03-31 22:28:37 +0800

    完成tabbar组件和tabbaritem组件的抽取

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,49 @@
 <template>
   <div id="app">
+    <tab-bar class="tabbar">
+      <tab-bar-item>
+        <img slot="itemIcon" src="./assets/img/tabbar/home.png" alt="">
+        <img slot="itemIcon-active" src="./assets/img/tabbar/home-active.png" alt="">
+        <div slot="itemName">首页</div>
+      </tab-bar-item>
+      <tab-bar-item>
+        <img slot="itemIcon" src="./assets/img/tabbar/category.png" alt="">
+        <img slot="itemIcon-active" src="./assets/img/tabbar/category-active.png" alt="">
+        <div slot="itemName">分类</div>
+      </tab-bar-item>
+      <tab-bar-item>
+        <img slot="itemIcon" src="./assets/img/tabbar/cart.png" alt="">
+        <img slot="itemIcon-active" src="./assets/img/tabbar/catr-active.png" alt="">
+        <div slot="itemName">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item>
+        <img slot="itemIcon" src="./assets/img/tabbar/profile.png" alt="">
+        <img slot="itemIcon-active" src="./assets/img/tabbar/profile-active.png" alt="">
+        <div slot="itemName">我的</div>
+      </tab-bar-item>
+    </tab-bar>
     <router-view/>
   </div>
 </template>
 
 <script>
+import TabBar from './components/tabbar/TabBar'
+import TabBarItem from './components/tabbar/TabBarItem'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    TabBar,
+    TabBarItem
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import "./assets/css/base.css";
+  .tabbar {
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;
+  }
 </style>
---
+++ src/components/tabbar/TabBarItem.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="tabbar-item" @click="itemClick">
+    <div v-if="!isActive">
+      <slot name='itemIcon'></slot>
+    </div>
+    <div v-else>
+      <slot name='itemIcon-active'></slot>
+    </div>
+    <div :class="{active: isActive}">
+      <slot name="itemName"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'TabBarItem',
+    data() {
+      return {
+        isActive: true
+      }
+    },
+    methods: {
+      itemClick() {
+        console.log('aaa');
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .tabbar-item {
+    flex: 1;
+    text-align: center;
+  }
+  .tabbar-item div {
+    margin-top: 3px;
+  }
+  .tabbar-item img {
+    width: 24px;
+    height: 24px;
+    vertical-align: middle;
+  }
+  .active {
+    color: pink;
+  }
+</style>
